781d98567c20ead7e8a81ca44f3a6c11303f9e74
dmun <davidru01@live.nl>
2024-07-12 01:14:29 +0200

    msaa

--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,10 @@ pub fn main() anyerror!void {
     const screenWidth = 1280;
     const screenHeight = 720;
 
+    rl.setConfigFlags(.{
+        .msaa_4x_hint = true,
+    });
+
     rl.initWindow(screenWidth, screenHeight, "particle-sim");
     defer rl.closeWindow();
 
@@ -14,6 +18,7 @@ pub fn main() anyerror!void {
     // Main game loop
     while (!rl.windowShouldClose()) {
         // Update
+        rl.drawCircle(screenWidth / 2, screenHeight / 2, 10, Color.red);
 
         // Draw
         rl.beginDrawing();
