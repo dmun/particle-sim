02ae5665593e97d50fa80fe111c8517d5fb40ff4
dmun <davidru01@live.nl>
2024-07-12 14:53:44 +0200

    progress

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,15 @@
 const std = @import("std");
 const rl = @import("raylib");
+const Vector2 = rl.Vector2;
 const Color = rl.Color;
+
+const gravity = 300;
+const collisionDamping = 0.7;
+
+const Particle = struct {
+    position: Vector2,
+    velocity: Vector2,
+};
 
 pub fn main() anyerror!void {
     const screenWidth = 1280;
@@ -15,14 +24,55 @@ pub fn main() anyerror!void {
 
     rl.setTargetFPS(60);
 
+    const allocator = std.heap.page_allocator;
+    var particles = std.ArrayList(Particle).init(allocator);
+
+    for (0..20) |ix| {
+        for (0..20) |iy| {
+            try particles.append(.{
+                .position = Vector2.init(
+                    screenWidth / 3 + @as(f32, @floatFromInt(ix)) * 20,
+                    screenHeight / 3 + @as(f32, @floatFromInt(iy)) * 20,
+                ),
+                .velocity = Vector2.zero(),
+            });
+        }
+    }
+
     // Main game loop
     while (!rl.windowShouldClose()) {
         // Update
-        rl.drawCircle(screenWidth / 2, screenHeight / 2, 10, Color.red);
+        const frametime = rl.getFrameTime();
+
+        for (particles.items) |*p| {
+            p.velocity = Vector2
+                .init(0, 1)
+                .scale(gravity)
+                .scale(frametime)
+                .add(p.velocity);
+
+            p.position = p.velocity
+                .scale(frametime)
+                .add(p.position);
+
+            if (@abs(p.position.x) > screenWidth) {
+                p.position.x = screenWidth * std.math.sign(p.position.x);
+                p.velocity.x *= -1 * collisionDamping;
+            }
+
+            if (@abs(p.position.y) > screenHeight) {
+                p.position.y = screenHeight * std.math.sign(p.position.y);
+                p.velocity.y *= -1 * collisionDamping;
+            }
+        }
 
         // Draw
         rl.beginDrawing();
         defer rl.endDrawing();
+
+        for (particles.items) |*p| {
+            rl.drawCircleV(p.position, 10, Color.red);
+        }
 
         rl.clearBackground(Color.black.brightness(0.1));
     }
